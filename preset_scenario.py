925cae5a2c2873c98a22e357f4850816d5e77397
Martje55555 <martje0629@gmail.com>
2022-05-16 13:53:41 -0500

    update

--- preset_scenario.py
+++ preset_scenario.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env python
 
-# Copyright (c) 2018 Intel Labs.
-# authors: German Ros (german.ros@intel.com)
-#
-# This work is licensed under the terms of the MIT license.
-# For a copy, see <https://opensource.org/licenses/MIT>.
-
-"""Example of automatic vehicle control from client side."""
 
 from __future__ import print_function
 
